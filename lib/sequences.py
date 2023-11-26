5400c9831fdf44f8fa895bf6f74557d85e08f55c
Ruth <ruro122020@gmail.com>
2023-11-25 21:00:32 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length == 0:
+        print(fib_list)
+    elif length == 1:
+        print([0])
+    else:
+        fib_list = [0, 1]
+        while len(fib_list) < length:
+            fib_list.append(fib_list[-1] + fib_list[-2])
+        print(fib_list)
+
+print_fibonacci(10)
